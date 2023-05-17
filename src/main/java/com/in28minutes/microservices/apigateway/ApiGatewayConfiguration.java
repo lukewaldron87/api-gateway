e379ee249fae593d9703e8189f9bf21dd5476145
Luke Waldron <lukewaldron87@gmail.com>
2023-05-17 14:40:46 +0200

    add mapping for seerive names

---
+++ src/main/java/com/in28minutes/microservices/apigateway/ApiGatewayConfiguration.java
@@ -0,0 +1,40 @@
+package com.in28minutes.microservices.apigateway;
+
+import org.springframework.cloud.gateway.route.Route;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.Buildable;
+import org.springframework.cloud.gateway.route.builder.PredicateSpec;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.function.Function;
+
+@Configuration
+public class ApiGatewayConfiguration {
+
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder builder){
+
+        return builder.routes()
+                .route( p -> p.path("/get")
+                        .filters(f -> f
+                                .addRequestHeader("MyHeader", "MyURI")
+                                .addRequestParameter("Param", "MyValue"))
+                        .uri("http://httpbin.org:80"))
+                // map the service name to the call i.e. "localhost:8765/currency-exchange/currency-exchange/from/USD/to/INR" becomes "localhost:8765/currency-exchange/from/USD/to/INR"
+                .route(p -> p.path("/currency-exchange/**")
+                        .uri("lb://currency-exchange"))
+                .route(p -> p.path("/currency-conversion/**")
+                        .uri("lb://currency-conversion"))
+                .route(p -> p.path("/currency-conversion-feign/**")
+                        .uri("lb://currency-conversion"))
+                // map a URL to another one
+                .route(p -> p.path("/currency-conversion-new/**")
+                        .filters(f -> f.rewritePath(
+                                "/currency-conversion-new/(?<segment>.*)",
+                                "/currency-conversion-feign/${segment}"))
+                        .uri("lb://currency-conversion"))
+                .build();
+    }
+}
